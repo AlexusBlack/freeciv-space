f31f3936441bfcab6703568d009d8c159fb52898
Alex Chernov <alexusblack@gmail.com>
2022-09-09 14:15:01 +1000

    installing space tileset as replacement for amplio2

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -284,6 +284,11 @@ cd "${basedir}"/freeciv && \
   cd build && ninja install || \
   handle_error 5 "Failed to install freeciv"
 
+echo "==== Downloading space tileset ===="
+git clone https://github.com/AlexusBlack/freeciv-space-tileset.git "${basedir}"/freeciv-space
+# Replacing Amplio2 tileset with Space tileset with minimum code changes
+cp "${basedir}"/freeciv-space/space.tilespec "${basedir}"/freeciv/freeciv/data/amplio2.tilespec
+cp -r "${basedir}"/freeciv-space/space "${basedir}"/freeciv/freeciv/data/
 
 echo "==== Building freeciv-web ===="
 if [ ! -f "${basedir}"/publite2/settings.ini ]; then
